88086083f59fe3c7421c25e70a7a87b965328ac6
torasan <grazinglion@gmail.com>
2025-10-16 12:48:27 +0000

    refactor: Remove CreditTesterNordy mapping from __init__.py - Updated NODE_CLASS_MAPPINGS to eliminate reference to CreditTesterNordy following its removal from the codebase.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,11 @@
-from .nodes import SaveImageS3PresignedUrlNordy, CreditTesterNordy
+from .nodes import SaveImageS3PresignedUrlNordy
 
 NODE_CLASS_MAPPINGS = {
     "SaveImageS3PresignedUrlNordy": SaveImageS3PresignedUrlNordy,
-    "CreditTesterNordy": CreditTesterNordy,
 }   
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SaveImageS3PresignedUrlNordy": "Save Image S3 PresignedUrl (Nordy)",
-    "CreditTesterNordy": "Credit Tester (Nordy)",
+    "SaveImageS3PresignedUrlNordy": "Save Image S3 PresignedUrl (Nordy)"
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
